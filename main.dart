0b930f1b8587395552c4d61c90147864cef2d72a
nqanh312 <111437622+nqanh312@users.noreply.github.com>
2023-01-14 16:37:36 +0700

    Create main.dart

---
+++ main.dart
@@ -0,0 +1,265 @@
+import 'dart:async';
+import 'dart:io';
+
+import 'package:camera/camera.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:dio/dio.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:file_picker/file_picker.dart';
+import 'package:open_file/open_file.dart';
+import 'package:qr_code_scanner/qr_code_scanner.dart';
+import 'package:image_picker/image_picker.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Obtain a list of the available cameras on the device.
+  final cameras = await availableCameras();
+
+  // Get a specific camera from the list of available cameras.
+  final firstCamera = cameras.first;
+
+  runApp(
+    MaterialApp(
+      theme: ThemeData(
+        // This is the theme of your application.
+        //
+        // Try running your application with "flutter run". You'll see the
+        // application has a blue toolbar. Then, without quitting the app, try
+        // changing the primarySwatch below to Colors.green and then invoke
+        // "hot reload" (press "r" in the console where you ran "flutter run",
+        // or simply save your changes to "hot reload" in a Flutter IDE).
+        // Notice that the counter didn't reset back to zero; the application
+        // is not restarted.
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(
+        // Pass the appropriate camera to the TakePictureScreen widget.
+        camera: firstCamera,
+      ),
+    ),
+  );
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.camera}) : super(key: key);
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final CameraDescription camera;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  final GlobalKey qrKey = GlobalKey(debugLabel: 'QR');
+  Barcode? result;
+  QRViewController? controller;
+  late Future<void> _initializeControllerFuture;
+  late CameraController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    // To display the current output from the Camera,
+    // create a CameraController.
+    _controller = CameraController(
+      // // Get a specific camera from the list of available cameras.
+      // widget.camera,
+      // // Define the resolution to use.
+      // ResolutionPreset.medium,
+
+      widget.camera,
+      ResolutionPreset.medium,
+      imageFormatGroup: ImageFormatGroup.yuv420,
+    );
+
+    // Next, initialize the controller. This returns a Future.
+    _initializeControllerFuture = _controller.initialize();
+  }
+
+  // In order to get hot reload to work we need to pause the camera if the platform
+  // is android, or resume the camera if the platform is iOS.
+  @override
+  void reassemble() {
+    super.reassemble();
+    if (Platform.isAndroid) {
+      controller!.pauseCamera();
+    } else if (Platform.isIOS) {
+      controller!.resumeCamera();
+    }
+  }
+
+  @override
+  void dispose() {
+    controller?.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: <Widget>[
+          SizedBox(
+            height: 100,
+            child: Row(
+              children: [
+                Flexible(flex: 10, child: Container(color: Colors.white)),
+              ],
+            ),
+          ),
+          SizedBox(
+            height: 500,
+            child: Row(
+              children: [
+                Flexible(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.white,
+                    )),
+                Flexible(
+                  flex: 8,
+                  child: QRView(
+                    key: qrKey,
+                    onQRViewCreated: _onQRViewCreated,
+                  ),
+                ),
+                Flexible(
+                    flex: 1,
+                    child: Container(
+                      color: Colors.white,
+                    )),
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: ElevatedButton(
+                onPressed: () async {
+                  // Take the Picture in a try / catch block. If anything goes wrong,
+                  // catch the error.
+                  try {
+                    // Ensure that the camera is initialized.
+                    await _initializeControllerFuture;
+                    print("1");
+
+                    // Attempt to take a picture and get the file `image`
+                    // where it was saved.
+                    final image = await _controller.takePicture();
+                    print("2");
+
+                    if (!mounted) return;
+                    print("3");
+
+                    // If the picture was taken, display it on a new screen.
+                    await Navigator.of(context).push(
+                      MaterialPageRoute(
+                        builder: (context) => DisplayPictureScreen(
+                          // Pass the automatically generated path to
+                          // the DisplayPictureScreen widget.
+                          imagePath: image.path,
+                        ),
+                      ),
+                    );
+                    print("abvvvvv");
+                  } catch (e) {
+                    // If an error occurs, log the error to the console.
+                    print("hello");
+                    print(e);
+                  }
+                },
+                child: Text('Take a photo'),
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Center(
+              child: (result != null)
+                  ? Text(
+                      'Barcode Type: ${describeEnum(result!.format)}   Data: ${result!.code}')
+                  : Text('Scan a code'),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  void _onQRViewCreated(QRViewController controller) {
+    this.controller = controller;
+    controller.scannedDataStream.listen((scanData) {
+      setState(() {
+        result = scanData;
+      });
+    });
+  }
+}
+
+/////////
+
+// A widget that displays the picture taken by the user.
+class DisplayPictureScreen extends StatelessWidget {
+  final String imagePath;
+
+  const DisplayPictureScreen({super.key, required this.imagePath});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: const Text('Want to upload file?')),
+        // The image is stored as a file on the device. Use the `Image.file`
+        // constructor with the given path to display the image.
+        body: Column(
+          children: [
+            TextButton(
+                onPressed: () {
+                  uploadFile(imagePath);
+                },
+                child: const Text("Upload File")),
+          ],
+        )
+        //Image.file(File(imagePath)),
+        );
+  }
+}
+
+Future uploadFile(String imagePath) async {
+  var dio = Dio();
+
+  //FilePickerResult? result = await FilePicker.platform.pickFiles();
+
+  if (imagePath != null) {
+    File file = File(imagePath);
+
+    String filename = file.path.split('/').last;
+
+    String filepath = file.path;
+
+    FormData data = FormData.fromMap({
+      'key': '44941129ae1905feaa538ae3a1ea3311',
+      'image': await MultipartFile.fromFile(filepath, filename: filename)
+    });
+
+    //upload to server
+    Directory directory = await getApplicationDocumentsDirectory();
+    var response = await dio.post("https://api.imgbb.com/1/upload", data: data,
+        onSendProgress: (int sent, int total) {
+      print('$sent, $total');
+    });
+    print(response.data);
+  } else {
+    print("Result is null");
+  }
+}
